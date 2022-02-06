e88aa10bdb09a1d4258c9315b1dedb829a922355
azizcherif1 <mohamedcherif45@gmail.com>
2022-02-06 13:22:58 +0100

    home+product

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import './Home.css'
+import Product from './Product';
 
 function Home() {
 
   return (
   <div className='home'>
       <div className='home_container'>
-          
-      </div>
-<p>MAIN PAGE</p>
+        <img 
+        className='home_image' src='https://serving.photos.photobox.com/14635621607c6863591886027321893fed57c9ecf619095f38910258b210eda3f0cc71e4.jpg'
+        alt=''
+        
+        />
+        <div className='home_row'>
 
-<h1>TEST TEST</h1>
+        <Product />
+        <Product />
+
+        </div>
+          
+        <div className='home_row'>
+          
+          
+          </div>
+
+
+          <div className='home_row'>
+            
+          
+        </div>
+      </div>
+
   </div>
   );
 }
---
+++ src/Product.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Product.css'
+
+function product() {
+  return (
+    <div className='product'>
+    <div className='product_info'>
+<p> VALORANT CARD 25$ </p>
+
+  <p className='product_price'>
+
+      <strong>75DT</strong>
+ 
+</p>
+<div className='product_rating'>
+
+  <p>★★★★★</p>
+
+  
+
+</div>
+
+
+    </div>
+    <img src='https://www.ubuy.com.pl/productimg/?image=aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL0kvODFWRTNPQWtFNkwuX1NMMTUwMF8uanBn.jpg
+  '
+  />
+  <button>Ajout au panier</button>
+
+
+</div>);
+}
+
+
+export default product;
